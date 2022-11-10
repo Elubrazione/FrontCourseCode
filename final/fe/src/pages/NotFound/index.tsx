06392d3ca95bacaa197404bf119eb4f737fcad54
jelly <1355532189@qq.com>
2022-11-10 16:11:19 +0800

    double reset and add login

--- final/fe/src/pages/NotFound/index.tsx
+++
@@ -1,6 +0,0 @@
-import "./index.css"
-const NotFound = () => {
-  return <div className="not-found">404</div>
-}
-
-export default NotFound
--- final/src/router.tsx
+++ final/src/router.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 import { createHashRouter } from "react-router-dom";
-import App from "./pages/App";
+import Login from "./pages/Login";
+import Main from "./pages/Main";
 import NotFound from "./pages/NotFound";
 
 const router = createHashRouter ([
     {   // 第一个界面，登录界面
         path: "/",
-        element: <App />,
+        element: <Login />,
         errorElement: <NotFound />,
         children: [
             {
                 // 登录成功，跳转到主页面
                 path: "",
+                element: <Main />
             }
         ]
-    },
-    {   // 第二个界面，
-        path: ""
     }
 ]);
+
+export default router;
