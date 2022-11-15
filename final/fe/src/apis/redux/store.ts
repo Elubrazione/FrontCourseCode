b1e6e980677a4b9345e3aeabb7131a01c3745487
jelly <1355532189@qq.com>
2022-11-15 09:08:59 +0800

    modify redux

--- final/fe/src/apis/redux/store.ts
+++ final/fe/src/apis/redux/store.ts
@@ -1,10 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import stuReducer from "./stuSlice";
 
 export const store = configureStore({
-    reducer: {
-        stu: stuReducer
-    }
+  reducer: {
+    student: stuReducer,
+  },
 });
 
-export default store;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- final/fe/src/apis/redux/stuSlice.ts
+++ final/fe/src/apis/redux/stuSlice.ts
@@ -4,24 +4,25 @@ import formDataType from "../dataTypes";
 const stuInfos: formDataType[] = [];
 
 export const stuSlice = createSlice({
-    name: "stu",
-    initialState: stuInfos,
-    reducers: {
-        addStu: state => {
-            state.unshift();
-        },
-        toggleStu: state => {
-
-        },
-        deleteStu: state => {
-
-        },
-        clearStus: state => {
-            
-        }
-    }
+  name: "stu",
+  initialState: stuInfos,
+  reducers: {
+		initStu: (state) => {},
+    addStu: (state) => {
+      state.unshift();
+    },
+    toggleStu: (state) => {},
+    deleteStu: (state) => {},
+    clearStus: (state) => {},
+  },
 });
 
-export const { addStu, toggleStu, deleteStu, clearStus } = stuSlice.actions;
+export const {
+	initStu,
+	addStu,
+	toggleStu,
+	deleteStu,
+	clearStus
+} = stuSlice.actions;
 
 export default stuSlice.reducer;
