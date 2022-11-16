a082615a25aef5e9d4e6e47e76e1a2cc5d2f3abf
jelly <1355532189@qq.com>
2022-11-16 11:59:56 +0800

    modify css

--- final/fe/src/pages/About/About.tsx
+++ final/fe/src/pages/About/About.tsx
@@ -1,5 +1,7 @@
 import { Layout } from "antd";
+import { Content } from "antd/lib/layout/layout";
 import React from "react";
+import "../../styles/styles.css";
 import HeadBar from "../../components/HeadBar";
 import SideBar from "../../components/SideBar";
 
@@ -9,6 +11,10 @@ const About = () => {
       <HeadBar />
       <Layout>
         <SideBar keki={"2"}/>
+        <Content>
+          <span className="about-text">1. 实现导航栏的折叠</span>
+          <span className="about-text">2. 可按照年级对表单进行排序</span>
+        </Content>
       </Layout>
     </>
   );
--- final/fe/src/pages/NotFound/NotFound.tsx
+++ final/fe/src/pages/NotFound/NotFound.tsx
@@ -1,9 +1,20 @@
 import "./NotFound.css";
+import { Button, Result } from "antd";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const navigate = useNavigate();
+  const linkToHome = () => {
+    navigate("/main");
+  };
   return (
-    <div>404</div>
+    <Result
+      className="notfound"
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+    />
   );
 };
 
